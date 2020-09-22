d1e58a1fcdc0ecc45687226876a7ad9648b2759d
omarzero130 <omarzero130@gmail.com>
2020-09-22 19:41:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hotel-Booking-System
+Laravel based application used to book rooms and bookings and manage bookings and clients.
+For now this is a basic booking management system for hotels.
+
+### Features
+* Adding clients and modify them and rooms associated with clients.
+* Adding a new Room or check the availability of the room.
+* Create and manage bookings
+* Search for clients by name
+
+
+
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+    // Dashboard
+    Route::resource('/','DashboardController');
+
+    // Clients
+    Route::resource('clients','ClientsController');
+    Route::get('/clients/data','ClientsController@data')->name('clients.data');
+
+    // Rooms
+    Route::resource('rooms','RoomController');
+
+    // Bookings
+    Route::resource('booking','BookingController');
+    // Cancel Bookings
+    Route::post('booking/{room_id}/{booking_id}','BookingController@cancel')->name('booking.cancel');
+
+    // Canceled Bookings
+    Route::get('bookings/canceled','BookingController@canceledBookings')->name('booking.canceled');
+
+    // Sessions
+    Route::get('/login','SessionsController@create')->name('login');
+    Route::post('/login','SessionsController@store');
+    Route::get('/logout','SessionsController@destroy');
+
+    // User
+    Route::get('/user','UserController@index');
+
+    Route::get('/room/available','RoomController@availableRooms');
+
+    Route::get('/room/not-available','RoomController@bookedRooms');
+
+    Route::post('/clients/search','ClientsController@searchClient');
